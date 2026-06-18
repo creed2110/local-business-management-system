7008d50535ba5b9f337da4048ca70166cedf479f
creed2110 <oyekanmigospel@gmail.com>
2026-06-18 17:19:00 +0100

    latest update

--- inventory-management-system/public/js/settings.js
+++ inventory-management-system/public/js/settings.js
@@ -1,32 +1,45 @@
 const LogoutModal = document.getElementById('LogoutModal');
 const ProfileModal = document.getElementById('ProfileModal');
 const ProfileForm = document.getElementById('ProfileForm');
-const CloseBtnA = document.getElementById('closeBtnA');
+const CloseBtnP = document.getElementById('closeBtnP');
 const CloseBtnV = document.getElementById('closeBtnV');
 const ViewBtn = document.querySelectorAll('.view-btn');
+const EditBtn = document.getElementById('editBtn');
+const LogoutBtn = document.getElementById('logoutBtn');
+const ViewModal = document.getElementById('viewModal');
 
 ViewBtn.forEach(button => {
     button.addEventListener('click', () => {
        viewModal.style.display ='block';
     });
 });
-CloseBtnV.addEventListener('click', () => {
+EditBtn.addEventListener('click', () => {
+    ProfileModal.style.display = "block";
+});
+LogoutBtn.addEventListener('click', () =>{
+    LogoutModal.style.display ="block"
+});
+
+CloseBtnV.addEventListener('click', () => { //Close btn for view modal
     viewModal.style.display ='none';
 });
 
-function addModal(){
-    ApplicationModal.style.display = 'block'
-}
-CloseBtnA.addEventListener('click', () =>{
-    ApplicationModal.style.display = "none";
+CloseBtnP.addEventListener('click', () =>{ //Close btn for Profile editing
+    ProfileModal.style.display = "none";
 });
 
+function Logout(){
+    window.location.href = "sigin.html"
+}
+function Return(){
+    LogoutModal.style.display = "none"
+}
 window.addEventListener('click', (e) =>{
     if(e.target === viewModal){
         viewModal.style.display ='none';
     }
     
-    if(e.target === ApplicationModal){
-        ApplicationModal.style.display ='none';
+    if(e.target === ProfileModal){
+        ProfileModal.style.display ='none';
     }
 } )
